93820be3ff362d220eee82cebcfb20c1262968ee
Vinícius Kegler <viniciuskegler.flexmobile@gmail.com>
2022-08-04 15:53:47 -0300

    Basic rest operations done.
    Put, post, get, update and delete created.

--- workshopmongodb/src/main/java/com/viniciuskegler/workshopmongodb/resources/UserResource.java
+++ workshopmongodb/src/main/java/com/viniciuskegler/workshopmongodb/resources/UserResource.java
@@ -1,13 +1,20 @@
 package com.viniciuskegler.workshopmongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.viniciuskegler.workshopmongodb.domain.User;
 import com.viniciuskegler.workshopmongodb.dto.UserDTO;
@@ -28,5 +35,31 @@ public class UserResource {
 		return ResponseEntity.ok().body(listDTO);
 	}
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
 	
+	@PostMapping
+	public ResponseEntity<Void> findById(@RequestBody UserDTO objDTO){
+		User obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> update(@RequestBody UserDTO objDTO, @PathVariable String id){
+		User obj = service.fromDTO(objDTO);
+		obj.setId(id);
+		service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
 }
--- workshopmongodb/src/main/java/com/viniciuskegler/workshopmongodb/services/UserService.java
+++ workshopmongodb/src/main/java/com/viniciuskegler/workshopmongodb/services/UserService.java
@@ -1,12 +1,15 @@
 package com.viniciuskegler.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.viniciuskegler.workshopmongodb.domain.User;
+import com.viniciuskegler.workshopmongodb.dto.UserDTO;
 import com.viniciuskegler.workshopmongodb.repository.UserRepository;
+import com.viniciuskegler.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +20,34 @@ public class UserService {
 	public List<User> findAll(){
 		return userRepo.findAll();
 	}
+
+	public User findById(String id){
+		Optional<User> obj = userRepo.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Object not found."));
+	}
+	
+	public User insert(User obj) {
+		return userRepo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		userRepo.deleteById(id);
+	}
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return userRepo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+	
 }
